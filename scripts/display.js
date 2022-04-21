c53b1490391fae29bd434597e665c23bc7048385
Aldrian Almonte <aldrianalmonte02@gmail.com>
2022-04-20 19:52:26 -0700

    Second push
    pets are being displayed as cards

--- scripts/display.js
+++ scripts/display.js
@@ -1,21 +1,41 @@
 //display the pets in list items
 function displayPet(){
-   
+   //creating the text
     let petsDiv= document.getElementById("pets");
-    for(let li=0;i<petSalon.pets.length;i++){
-        let pet = petSalon.pets[i];
-        let li = document.createElement("li");//creating the li tag
-        let text = document.createTextNode(pet.name);
-        li.appendChild(text);
-        petsDiv.appendChild(li);
-    }
+    // petsDiv.innerHTML=""; c//clear field
+    let tmp=""
+    for(let i=0;i<petSalon.pets.length;i++){
+        let pet = petSalon.pets[i];//get each pet
+        tmp+=`<li>${pet.petName} --- ${pet.age}</li>`//concatenation
+        console.log(tmp);
 
-    
-    li.appendChild(text);
-    petsDiv.appendChild(li);
+        // let li = document.createElement("li");//creating the li tag
+        // let text = document.createTextNode(pet.name);
+        // li.appendChild(text);
+        // petsDiv.appendChild(li);
+    }
+    document.getElementById("pets").innerHTML=tmp;
 }
 
 //display the pets in cards
+function displayCards(){
+    let tmp="";
+    for(let i=0; i<petSalon.pets.length;i++){
+        let pet = petSalon.pets[i];
+        tmp+=`
+        <div class="pet">
+            <h4>${pet.petName}</h4>
+            <p>Age:${pet.age}</p>
+            <p>${pet.gender}</p>
+            <p>${pet.petBreed}</p>
+            <p>${pet.serviceProvided}</p>
+            <p>${pet.humanName}</p>
+            <p>${pet.contactInfo}</p>
+            </div>
+        `
+    }
+    document.getElementById("pets").innerHTML=tmp;
+}
 
 
 //display pets in table
--- scripts/register.js
+++ scripts/register.js
@@ -29,21 +29,49 @@ function Pet(pname,age,gender,pbreed,service,hname,cinfo){
 
 //get the info from the inputs and store the info
 let inputName=document.getElementById("txtName");
-let inputAge=document.getAnimations("txtAge");
+let inputAge=document.getElementById("txtAge");
 let inputGender=document.getElementById("txtGender");
 let inputBreed=document.getElementById("txtBreed");
 let inputService=document.getElementById("selService");
 let inputHname=document.getElementById("txtHname");
 let inputCinfo=document.getElementById("txtCinfo");
 
+
+
+function isValid(aPet){
+    //return false when the pet is not valid
+    //return true if the pet is valid
+    let valid=true;
+    if(aPet.petName.length==0){
+        //if we arrived here it means that the name is not valid
+        valid=false;
+        console.error("Invalid Name")
+    if(aPet.serviceProvided.length==0){
+        valid=false;
+        console.error("Invalid Service")
+    if(aPet.contactInfo.length==0){
+        valid=false;
+        console.error("Invalid Contact Info")
+    }
+    }
+    }
+
+    return valid;//it could be true or false
+}
+
 function register(){
     //create the pet
     let thePet = new Pet (inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value,inputHname.value,inputCinfo.value);
-    console.log(thePet);
+    if(isValid(thePet)==true){
+       petSalon.pets.push(thePet)
+        displayCards();
+        //push the pet into the array
 
-    //push the pet into the array
-
-    //clear the inputs
+        //clear the inputs
+        clearInputs(); 
+    }
+   
+    
 }
 
 function clearInputs(){
@@ -61,5 +89,7 @@ let Scrappy = new Pet ("Scrappy",7,"Bulldog","nails","Alex","253-255-2555");
 let Clifford = new Pet ("Clifford",12,"Red","full treatment","Aldrian","786-888-5225");
 let Crystal = new Pet ("Crystal",8,"Pug","VIP","Ruby","206-565-2564");
 
+petSalon.pets.push(Scrappy,Clifford,Crystal)
+displayCards();
 
 // console.log(pet1,pet2,pet3)
